76f99046c41f331eaea204d7db87f4735166e323
Meetprajapati07 <mppm458@gmail.com>
2025-03-21 13:28:48 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3 ) // we are using node js, not browser
+
+console.log(3 + 3);
+// code readability should be high
+console.log("Meet");
+
+//number => 2 to Power 33
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value or value  not asign
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // type of undefined
+console.log(typeof null); // type of object
---
+++ variables.js
@@ -0,0 +1,16 @@
+const accountId = 14255
+let accountemail = "pmeet8804@gmail.com"
+var accountpassword = "123456"
+accountcity = "delhi"
+
+/*
+Prefer Not To Use Var
+because of issue in block scope and functional scope
+*/
+
+let accountstate;
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate]);
+
+
+
+
